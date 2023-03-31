a696ad694b6e72cfd950212c6f1bf175dde2258d
IoT-林柏諺[Michael.Lin] <a09755065@gmail.com>
2023-03-31 10:11:42 +0800

    update

--- src/Calendar/ToDoList.js
+++ src/Calendar/ToDoList.js
@@ -7,13 +7,13 @@ function ToDoList({today}) {
 
     // 點擊當日事項後移除該事項
     const handleClick = event => {
-        const newArray = note.filter(obj => {
-            return obj.date !== event.target.className})
+        const newArray = note.filter(obj => {    // filter回傳一個包含符合項目的array
+            return obj.date !== event.target.className});
         setNote(newArray);
     }
     
     // 將note中的date的value與日曆的日期做比對，用以將對應的物件(result)渲染在對應日期
-    const result = note.filter(obj => {    // filter回傳一個包含符合項目的array
+    const result = note.filter(obj => {    
         return obj.date === today
     });
 
@@ -22,8 +22,11 @@ function ToDoList({today}) {
     }
     else {
         return (
-            <div className={today} style={{color: "rgb(0, 0, 0)"}} onClick={handleClick}>
-                {result.map((value, index) => <p key={index} style={{margin: 0}}>{value.content}</p>)}
+            <div style={{color: "rgb(0, 0, 0)"}}>
+                {result.map((value, index) => <p className={today} key={index} style={{margin: 0}} onClick={handleClick}>
+                    {value.content}
+                    </p>
+                    )}
             </div>
             );
     }
